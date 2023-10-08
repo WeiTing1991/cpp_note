f9013a5fb9e188a7ea1c543851994d598ac16d6e
WeitingChen <weitingchen0502@gmail.com>
2023-10-09 00:43:39 +0200

    intial commit from linux

--- cpp_cmake/src/main.cpp
+++ cpp_cmake/src/main.cpp
@@ -3,7 +3,8 @@
 
 int main(){
     
-    std::cout << "First test compiling by g++" << std::endl;
+    std::cout << "First test compiling by g++ in Windows"<< std::endl;
+    std::cout << "First test compiling by g++ in Ubuntu" << std::endl;
     return 0;
 
 }
---
+++ opgl_cpp_linux/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+
+    std::cout << "first test" << std::endl;
+
+    return 0;
+}
